59e68924c21f706d33e4e39e281263b3ecf5d0fa
Yussef Mrt <52961281+TheXtn@users.noreply.github.com>
2021-07-03 17:13:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
 
 
   return(
-<Provider session={pageProps.session}>
+
 <ChakraProvider>
 
 <Lay>{loading?<Progress size="xs" isIndeterminate />:<Component {...pageProps} />}</Lay>
@@ -44,7 +44,7 @@ function MyApp({ Component, pageProps }) {
 
 
 </ChakraProvider>
-</Provider>
+
       )
 
 }
